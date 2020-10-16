1f4985f43d17c83f0533a37f1773b459633245ab
jkwk539 <jkwk539@gmail.com>
2020-10-16 12:34:37 -0400

    5 files modified, 6 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,19 +1,10 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: 'Kevin Jiang',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'My Resume'
   },
-  {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
-  }
+
 ];
 
 
